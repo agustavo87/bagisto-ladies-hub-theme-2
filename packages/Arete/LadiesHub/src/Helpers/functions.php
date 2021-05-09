ef93714753acc4784d23d943d462f290ba52039e
Gustavo Ayala <agustavo87@gmail.com>
2021-05-08 21:28:38 -0300

    changed functions

--- packages/Arete/LadiesHub/src/Helpers/functions.php
+++ packages/Arete/LadiesHub/src/Helpers/functions.php
@@ -4,42 +4,52 @@ declare(strict_types = 1);
 
 namespace Arete\LadiesHub\Helpers;
 
+if  (!function_exists( __NAMESPACE__ . '\difference')) {
 
-function difference () {
-    static $start = null;
-    if(!$start) $start = now();
-    return $start->diff(now());
-}
-
-/**
- * @param \Webkul\Attribute\Models\AttributeFamily $attributeFamily
- * @return \Illuminate\Support\Collection
- */
-function getFamilyAttributes($attributeFamily)
-{
-    $attributes = collect();
-    $attributeGroups = $attributeFamily->attribute_groups;
-
-    foreach ($attributeGroups as $attributeGroup) {
-        foreach ($attributeGroup->custom_attributes as $customAttribute) {
-            $attributes->push($customAttribute);
-        }
+    function difference () {
+        static $start = null;
+        if(!$start) $start = now();
+        return $start->diff(now());
     }
 
-    return $attributes;
 }
 
-/**
- * Get the element of an array if it exist.
- * 
- * reutrn null otherwise
- * 
- * @param mixed $key
- * @param array $array
- * 
- * @return mixed
- */
-function getIfExist($key, array $array)
-{
-    return array_key_exists($key, $array) && $array[$key];
+if  (!function_exists( __NAMESPACE__ . '\getFamilyAttributes')) {
+
+    /**
+     * @param \Webkul\Attribute\Models\AttributeFamily $attributeFamily
+     * @return \Illuminate\Support\Collection
+     */
+    function getFamilyAttributes($attributeFamily)
+    {
+        $attributes = collect();
+        $attributeGroups = $attributeFamily->attribute_groups;
+
+        foreach ($attributeGroups as $attributeGroup) {
+            foreach ($attributeGroup->custom_attributes as $customAttribute) {
+                $attributes->push($customAttribute);
+            }
+        }
+
+        return $attributes;
+    }
+
+}
+
+
+if  (!function_exists( __NAMESPACE__ . '\getIfExist')) {
+    /**
+     * Get the element of an array if it exist.
+     * 
+     * reutrn null otherwise
+     * 
+     * @param mixed $key
+     * @param array $array
+     * 
+     * @return mixed
+     */
+    function getIfExist($key, array $array)
+    {
+        return array_key_exists($key, $array) && $array[$key];
+    }
 }
